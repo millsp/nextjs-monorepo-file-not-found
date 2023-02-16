15740fbd4d886dfcbbc2fdbb77ebe8091d7b9645
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-02-16 12:11:51 -1000

    bump nextjs to canary

--- packages/service/next.config.js
+++ packages/service/next.config.js
@@ -4,12 +4,12 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals = [...config.externals, 'file-reader']
+  // experimental: {
+  //   outputFileTracingRoot: path.join(__dirname, '../../'),
+  // },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.externals = [...config.externals, 'file-reader']
 
-    return config
-  },
+  //   return config
+  // },
 }
