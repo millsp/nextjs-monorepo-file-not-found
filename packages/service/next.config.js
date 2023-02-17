ca1d49ef01d7fdbbbc9e4ceac5fe89058bc49f5d
Jan Piotrowski <piotrowski+github@gmail.com>
2023-02-17 18:24:59 +0000

    remove old workaround

--- packages/service/next.config.js
+++ packages/service/next.config.js
@@ -4,12 +4,8 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
-  // experimental: {
-  //   outputFileTracingRoot: path.join(__dirname, '../../'),
-  // },
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   config.externals = [...config.externals, 'file-reader']
-
-  //   return config
-  // },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals = [...config.externals, 'file-reader']
+    return config
+  },
 }
