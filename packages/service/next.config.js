ff13ff9a6bab5dc50bfc1066cd62478b7f6f01c1
Jan Piotrowski <piotrowski+github@gmail.com>
2023-02-17 18:25:11 +0000

    oops

--- packages/service/next.config.js
+++ packages/service/next.config.js
@@ -4,8 +4,8 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals = [...config.externals, 'file-reader']
-    return config
-  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.externals = [...config.externals, 'file-reader']
+  //   return config
+  // },
 }
